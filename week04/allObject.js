4d888b8bf330ef70d4c8742fe9df7d6f71d80de9
swpuhu <swpujita@126.com>
2020-05-09 14:17:44 +0800

    visualize

--- week04/allObject.js
+++ week04/allObject.js
@@ -55,49 +55,94 @@ let queue = [];
 for (let key of globalProperty) {
     queue.push({
         path: [key],
-        object: this[key]
+        object: this[key],
+        parent: null,
     });
 }
 
 let current;
 let set = new Set();
 let count = 0;
+let nodes = [];
+let edges = [];
+let id = 0;
+let level = 0;
 while (queue.length) {
-    
-    current = queue.shift();
-    if (set.has(current.object)) {
-        continue;
+    let size = queue.length;
+    while (size--) {
+        current = queue.shift();
+        if (set.has(current.object)) {
+            continue;
+        }
+        set.add(current.object);
+        current.id = id++;
+        current.pathName = current.path.join('.');
+        nodes.push({
+            id: current.id,
+            label: current.path[current.path.length - 1],
+            class: `level${level}`
+        });
+        if (current.parent) {
+            edges.push({
+                label: `e${current.parent.id}-${current.id}`,
+                source: current.parent.id.toString(),
+                target: current.id.toString(),
+                weight: 1
+            });
+        }
+        // console.log(current.path.join('.'));
+        if (current.object == undefined) continue;
+        ++count;
+        for (let p of Object.getOwnPropertyNames(current.object)) {
+            let property = Object.getOwnPropertyDescriptor(current.object, p);
+
+            if (property.hasOwnProperty('value') &&
+                ((property.value != null) && (typeof property.value == 'object' || typeof property.value == 'function')) &&
+                property.value instanceof Object) {
+                queue.push({
+                    path: current.path.concat([p]),
+                    object: property.value,
+                    parent: current
+                });
+            }
+
+            if (property.hasOwnProperty('get') && property.get instanceof Object) {
+                queue.push({
+                    path: current.path.concat([p]),
+                    object: property.get,
+                    parent: current
+                });
+
+            }
+
+            if (property.hasOwnProperty('set') && property.set instanceof Object) {
+                queue.push({
+                    path: current.path.concat([p]),
+                    object: property.set,
+                    parent: current
+                });
+
+            }
+        }
     }
-    set.add(current.object);
-    console.log(current.path.join('.'));
-    if (current.object == undefined) continue;
-    ++count;
-    for (let p of Object.getOwnPropertyNames(current.object)) {
-        let property = Object.getOwnPropertyDescriptor(current.object, p);
-
-        if (property.hasOwnProperty('value') && 
-        ((property.value != null) && (typeof property.value == 'object' || typeof property.value == 'function')) && 
-        property.value instanceof Object) {
-            queue.push({
-                path: current.path.concat([p]),
-                object: property.value
-            });
-        }
-
-        if (property.hasOwnProperty('get') && property.get instanceof Object) {
-            queue.push({
-                path: current.path.concat([p]),
-                object: property.get
-            });
-            
-        }
-
-        if (property.hasOwnProperty('set') && property.set instanceof Object) {
-            queue.push({
-                path: current.path.concat([p]),
-                object: property.set
-            });
-            
-        }
-    }
+    level++;
 }
+
+// set.add(globalThis);
+
+nodes.forEach(node => {
+    node.style = {
+        lineWidth: 1,
+        stroke: '#666',
+        fill: 'steelblue',
+        color: '#000'
+    };
+    node.type = 'circle';
+    node.size = 30;
+})
+// console.log(nodes);
+// console.log(edges);
+
+let data = {nodes, edges};
+console.log(data);
+
