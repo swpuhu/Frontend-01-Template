81bc767d990643c4ce27a9c0ab9d2bec53b5903d
huyunhe <swpujita@126.com>
2020-05-21 06:55:22 +0800

    update parser

--- week06/parser.js
+++ week06/parser.js
@@ -1,47 +1,153 @@
 const EOF = Symbol('EOF');
+const css = require('css');
 let state = data;
 let returnState;
 let token = null;
 let tempBuffer = '';
 let lastToken;
-let stack = [];
+let stack = [{type: 'document', children: []}];
 let text = '';
 let totalText = '';
-function emit(token) {
-    console.log(token);
-    
-    lastToken = token;
-    if (token.type === 'startTag' && !token.isSelfClosingTag) {
-        token.children = [];
-        let top = stack[stack.length - 1];
-        if (top) {
-            if (text) {
-                top.children.push(text);
-                text = '';
+let cssText = '';
+let startCss = false;
+let rules = [];
+function addCSSRule (text) {
+    let ast = css.parse(text);
+    rules.push(...ast.stylesheet.rules);
+}
+
+function match (element, selector) {
+    if (!selector || !element.attributes)
+        return false;
+    if (selector.charAt(0) === '#') {
+        let attr = element.attributes.filter(attr => attr.name === 'id')[0]
+        if (attr && attr.value === selector.replace('#', '')) {
+            return true;
+        } else if (selector.charAt(0) === '.') {
+            let attr = element.attributes.filter(attr => attr.name === 'class')[0];
+            if (attr && attr.value === selector.replace('.', ''))
+                return true;
+        } else {
+            if (element.tagName === selector) {
+                return true;
             }
-            top.children.push(token);
         }
-        stack.push(token);
-    } else if (token.isSelfClosingTag) {
-        let top = stack[stack.length - 1];
-        if (top) {
-            top.children.push(token);
-        }
-    } else if (token.type === 'endTag') {
-        if (text) {
-            stack[stack.length - 1].children.push(text);
-            text = '';
-        }
-        let obj = stack.pop();
-        if (!stack.length) {
-            console.log(obj);
-            
-        }
-    } else if (token.type === 'text') {
-        text += token.content;
-        totalText += token.content;
     }
-    // console.log(token);
+}
+
+function specificity (selector) {
+    let p = [0, 0, 0, 0];
+    let selectorParts = selector.split(' ');
+    for (let part of selectorParts) {
+        if (part.charAt(0) === '#') {
+            p[1] += 1;
+        } else if (part.charAt(0) === '.') {
+            p[2] += 1;
+        } else {
+            p[3] += 1;
+        }
+    }
+    return p;
+}
+
+function compare(sp1, sp2) {
+    if (sp1[0] - sp2[0]) {
+        return sp1[0] - sp2[0];
+    }
+    if (sp1[1] - sp2[1]) {
+        return sp1[1] - sp2[1];
+    }
+    if (sp1[2] - sp2[2]) {
+        return sp1[2] - sp2[2];
+    }
+
+    return sp1[3] - sp2[3];
+}
+
+function computeCSS (element) {
+    let elements = stack.slice().reverse();
+    if (!element.computedStyle) {
+        element.computedStyle = {};
+    }
+
+    for (let rule of rules) {
+        let selectorParts = rule.selectors[0].split(' ').reverse();
+
+        if (!match(element, selectorParts[0]))
+            continue;
+        
+            let j = 1;
+            for (let i = 0; i < elements.length; i++) {
+                if (match(elements[i], selectorParts[j])) {
+                    j++;
+                }
+            }
+
+            if (matched) {
+                let sp = specificity(rule.selectors[0]);
+                let computedStyle = element.computedStyle;
+                for (let declaration of rule.declarations) {
+                    if (!computedStyle[declaration.property]) {
+                        computedStyle[declaration.property] = {};
+                    }
+
+                    if (!computedStyle[declaration.property].specificity) {
+                        computedStyle[declaration.property].value = declaration.value;
+                        computedStyle[declaration.property].specificity = sp;
+                    } else if (compare(computedStyle[declaration.property].specificity, sp) < 0) {
+                        computedStyle[declaration.property].value = declaration.value;;
+                        computedStyle[declaration.property].specificity = sp;
+                    }
+                }
+            }
+    }
+}
+
+
+function emit(token) {
+    let top = stack[stack.length - 1];
+    if (token.type === 'startTag') {
+        console.log(token);
+        
+        let element = {
+            type: 'element',
+            children: [],
+            attributes: []
+        };
+
+        element.tagName = token.tagName;
+
+        computeCSS(element);
+        if (!top.children) {
+            debugger;
+        }
+        top.children.push(element);
+
+        if (!token.isSelfClosingTag) {
+            stack.push(element);
+        }
+
+        currentTextNode = null;
+    } else if (token.type == 'endTag') {
+        if (top.tagName != token.tagName) {
+            throw new Error('Tag start end doesn\'t match');
+        } else {
+            if (top.tagName === 'style') {
+                addCSSRule (top.children[0].content);
+            }
+            stack.pop();
+        }
+        currentTextNode = null;
+    } else if (token.type ===' text') {
+        if (currentTextNode == null) {
+            currentTextNode = {
+                type: 'text',
+                content: ""
+            };
+            top.children.push(currentTextNode);
+        }
+        currentTextNode.content += token.content;
+    }
 }
 
 function isAppropriateEndTagToken(token, lastToken) {
@@ -1071,3 +1177,6 @@ body div img{
 `
 
 parse(testHtml);
+console.log(cssText);
+let ast = css.parse(cssText);
+console.log(JSON.stringify(ast, null, "    "));
