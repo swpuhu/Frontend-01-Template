4169015de5c1e2b7cc576fd25527da1af892cba4
huyunhe <swpujita@126.com>
2020-07-15 22:19:34 +0800

    migrate drag operation to jsx

--- week14/component/main2.jsx
+++ week14/component/main2.jsx
@@ -30,7 +30,7 @@ class Carousel {
     render() {
         let children = this.data.map(url => {
             let element = <img src={url} />
-            element.addEventListener('dragstart', e => false);
+            element.addEventListener('dragstart', e => e.preventDefault());
             return element;
         });
         let root = <div class="carousel">
@@ -62,8 +62,72 @@ class Carousel {
         }
 
         setTimeout(nextPic, 3000);
+        root.addEventListener('mousedown', (e) => {
+            let startX = e.clientX, startY = e.clientY;
 
-        return root
+            let lastPosition = (position - 1 + this.data.length) % this.data.length;
+            let nextPosition = (position + 1) % this.data.length
+
+            let current = children[position];
+            let last = children[lastPosition];
+            let next = children[nextPosition];
+
+
+            current.style.transition = 'ease 0s';
+            last.style.transition = 'ease 0s';
+            next.style.transition = 'ease 0s';
+
+
+            const move = (ev) => {
+
+                current.style.transition = 'ease 0s';
+                last.style.transition = 'ease 0s';
+                next.style.transition = 'ease 0s';
+
+                current.style.transform = `translateX(${-500 * position}px)`;
+                last.style.transform = `translateX(${-500 - 500 * lastPosition}px)`;
+                next.style.transform = `translateX(${500 - 500 * nextPosition}px)`;
+
+
+                let offsetX = ev.clientX - startX;
+                let offsetY = ev.clientY - startY;
+
+                current.style.transform = `translateX(${offsetX - 500 * position}px)`;
+                last.style.transform = `translateX(${offsetX - 500 - 500 * lastPosition}px)`;
+                next.style.transform = `translateX(${offsetX + 500 - 500 * nextPosition}px)`;
+            }
+
+            const up = (ev) => {
+
+
+                let offset = 0;
+                let offsetX = ev.clientX - startX;
+                if (offsetX > 250) {
+                    offset = 1;
+                } else if (offsetX < -250) {
+                    offset = -1;
+                }
+
+                current.style.transition = '';
+                last.style.transition = '';
+                next.style.transition = '';
+
+
+                current.style.transform = `translateX(${offset * 500 - 500 * position}px)`;
+                last.style.transform = `translateX(${offset * 500 - 500 - 500 * lastPosition}px)`;
+                next.style.transform = `translateX(${offset * 500 + 500 - 500 * nextPosition}px)`;
+
+                position = (position - offset + this.data.length) % this.data.length;
+                console.log(position);
+
+                document.removeEventListener('mousemove', move);
+                document.removeEventListener('mouseup', up);
+            }
+
+            document.addEventListener('mousemove', move);
+            document.addEventListener('mouseup', up);
+        })
+        return root;
     }
 
     mountTo(parent) {
